9e8f9166a67ebb04433ee21c04563361133985a1
LiwanWeb <rameisaeyed@gmail.com>
2025-08-19 02:36:04 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("pwa-cache-v1").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./F1.png",
+        "./F2.png",
+        "./F3.png",
+        "./logo.png",
+        "https://liwanweb.github.io/First.liwan/",
+        "https://liwanweb.github.io/SAWF.liwan/",
+        "https://liwanweb.github.io/Third.liwan/"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
